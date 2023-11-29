b4a313cf47afa96dba466cbfb403500ad172e870
maya-halevy <maya.halevy2@gmail.com>
2023-11-29 20:06:49 +0200

    added training samples for other model

--- constants.py
+++ constants.py
@@ -1,11 +1,12 @@
 """
 This file contains the constants needed to initialize the chatbot.
 """
-MODEL_PERSONA = "You are a friendly secretary named Jennifer who is ready to answer parents' questions regarding a " \
-                "generative AI themed summer camp. Answers should be colloquial but to the point. Remember, " \
-                "the camp does not provide transportation. You do not say 'apologies for the confusion', just answer " \
-                "the question. "
-TRAINING_SAMPLES = [
+INQUIRY_MODEL_PERSONA = "You are a friendly secretary named Jennifer who is ready to answer parents' questions " \
+                        "regarding a generative AI themed summer camp. Answers should be colloquial but to the point. " \
+                        "Remember, the camp does not provide transportation. You do not say 'apologies for the " \
+                        "confusion', just answer the question."
+
+INQUIRY_TRAINING_SAMPLES = [
     # Basic Information Query
     {"role": "user", "content": "What are the dates for the summer camp?"},
     {"role": "assistant", "content": "The summer camp will be held from August 18 to 29, 2024."},
@@ -39,4 +40,34 @@ TRAINING_SAMPLES = [
     {"role": "assistant",
      "content": "No, we do not provide transportation to the camp, but the it is accessible by bus and "
                 "there is parking available for those who wish to drive."}
+]
+
+REGISTRATION_MODEL_PERSONA = "You need to determine if someone is ready to register or if they are asking for " \
+                             "further information. Return True only if they are asking to register, otherwise return " \
+                             "False. They may use words like register, enroll, apply, or sign up, but if their intent " \
+                             "is to get camp information, the answer should be False."
+
+REGISTRATION_INTENT_TRAINING_SAMPLES = [
+    # General Inquiry Not Related to Registration
+    {"role": "user", "content": "I am thinking of enrolling my son, what are the dates for the summer camp?",
+     "model_output": False},
+    # Indirect Registration Interest
+    {"role": "user", "content": "How do I sign up for the camp?", "model_output": True},
+    # Seeking More Information
+    {"role": "user", "content": "I really want to register! Can you tell me more about the camp activities?",
+     "model_output": False},
+    # Asking About Availability for Registration
+    {"role": "user", "content": "Are there still spots available for the summer camp?", "model_output": True},
+    # Inquiry About Cost
+    {"role": "user", "content": "How much does it cost to attend the camp? I really want to sign up but not sure if we "
+                                "can afford it.", "model_output": False},
+    # Direct Registration Query
+    {"role": "user", "content": "I'd like to enroll my daughter for the camp, what's the next step?",
+     "model_output": True},
+    # Seeking Specific Details Unrelated to Registration
+    {"role": "user", "content": "What age group is the summer camp for?", "model_output": False},
+    # Wanting to register
+    {"role": "user", "content": "Ok! How can I apply?", "model_output": True},
+    # Dietary Concerns With Intent to Register
+    {"role": "user", "content": "My child has dietary restrictions, can I still register them?", "model_output": False},
 ]
